09ba14a435b527d1f3ff04d011a222c0a1bb1c6e
Yiannis Kryfos <yianniskryfos@yahoo.com>
2020-12-19 21:09:17 +0200

    Added DI and EF Core.

---
+++ BoilerplateConsole/Data/BoilerplateConsoleDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoilerplateConsole.Data
+{
+    public class BoilerplateConsoleDbContext : DbContext
+    {
+        public BoilerplateConsoleDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- BoilerplateConsole/Program.cs
+++ BoilerplateConsole/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using BoilerplateConsole.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
 
 namespace BoilerplateConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await Console.Out.WriteLineAsync("Hello World (from advanced template)!");
+
+
+            Console.ReadLine();
         }
+
+        static IHostBuilder CreateDefaultBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+		        services.AddDbContext<BoilerplateConsoleDbContext>(options =>
+		        {
+			        options.UseSqlServer(context.Configuration.GetConnectionString("DefaultConnection"));
+		        }
+		           ));
     }
 }
